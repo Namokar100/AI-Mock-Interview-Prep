d661f3affb3dc4f3e25207d15a004772ae1d2f8b
Namokar100 <namokarsvg@gmail.com>
2025-03-26 00:10:00 +0530

    designed homepage, firebase set up.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,48 @@
+import InterviewCard from '@/components/InterviewCard'
+import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+        <section className='card-cta'>
+          <div className='flex flex-col gap-6 max-w-lg'>
+            <h2 className='text-primary-100'>Get ready for your next interview</h2>
+            <p className='text-lg'>Practice with a mock interview partner</p>
+
+            <Button asChild className='btn-primary max-sm:w-full'>
+              <Link href="/interview">Start an Interview</Link>
+            </Button>
+          </div>
+
+          <Image src='/robot.png' alt='robot dude' width={400} height={400} />
+        </section>
+
+        <section className='flex flex-col gap-6 mt-8'>
+            <h2 className='text-center'>Your Interviews</h2>
+
+            <div className='interview-section max-sm:w-8/10 mx-auto flex flex-row gap-6 flex-wrap justify-center'>
+                {dummyInterviews.map((interview) => (
+                    <InterviewCard key={interview.id} {...interview} />
+                ))}
+                {/* <p>You havent taken any interviews yet</p> */}
+            </div>
+        </section>
+
+        <section className='flex flex-col gap-6 mt-8'>
+          <h2 className='text-center'>Take an Interview</h2>
+
+          <div className='interview-section max-sm:w-8/10 mx-auto flex flex-row gap-6 flex-wrap justify-center'>
+                {dummyInterviews.map((interview) => (
+                    <InterviewCard key={interview.id} {...interview} />
+                ))}
+            {/* <p>There are no interviews available at the moment</p> */}
+          </div>
+        </section>
+    </>
   )
 }
 
---
+++ firebase/client.ts
@@ -0,0 +1,25 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJLvCJFUcJpH2XpBpgKd1MXT3jpLN_cUk",
+  authDomain: "ai-interview-prep-34b8a.firebaseapp.com",
+  projectId: "ai-interview-prep-34b8a",
+  storageBucket: "ai-interview-prep-34b8a.firebasestorage.app",
+  messagingSenderId: "882466045539",
+  appId: "1:882466045539:web:d5814090841f8705f18256",
+  measurementId: "G-1P2KZJEB0J"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
